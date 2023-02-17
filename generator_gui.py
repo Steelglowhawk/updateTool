5ee4a2889c85ffb6cbba505f83e815e9150e20ef
Steelglowhawk <steel_hawk@list.ru>
2023-02-17 06:02:52 +0500

    Update generator_gui.py
    Модификации для минимального запуска.
    Косметические исправления.
    Размер окна изменен с фиксированного на минимальный (длинные названия каталога не вмещаются). 
    Отключен цвет фона.

--- generator_gui.py
+++ generator_gui.py
@@ -1,19 +1,22 @@
 import sys
 import os
+import pathlib
 import generator_func
 from time import sleep
 import random
 
 from PyQt6.QtCore import Qt, QObject, QThread, pyqtSignal, QThreadPool
-from PyQt6.QtWidgets import (
-    QApplication,
-    QLabel,
-    QMainWindow,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-    QFileDialog,
-    QStatusBar
+from PyQt6.QtWidgets import (QApplication,
+                             QLabel,
+                             QMainWindow,
+                             QPushButton,
+                             QVBoxLayout,
+                             QWidget,
+                             QFileDialog,
+                             QGridLayout,
+                             QLineEdit,
+                             QProgressBar,
+                             QStatusBar
 )
 
 
@@ -34,15 +37,54 @@ class Worker(QObject):
         pass
 
 
-
-
 class Window(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.clicksCount = 0
-        self.setupUi()
-        self.get_dir = '/Users/steelhawk/PycharmProjects/UpdateTool/files'
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle("Генератор ИК")  # заголовок главного окна
+        self.setMinimumSize(400, 150)  # минимальные размеры главного окна
+        self.get_directory_path = QPushButton('Выбор каталога', self)
+        self.create_IK = QPushButton('Создать конверты', self)
+        self.iteration_label = QLabel()
+        self.date_label = QLabel()
+        self.directory_path = QLineEdit()
+        self.converts = QLineEdit()
+        self.input_date = QLineEdit()
+        self.progressbar = QProgressBar()
+        self.get_directory_path.clicked.connect(self.get_directory)
+        # написать функцию для созданию конвертов??? См. строка ниже
+        # self.create_IK.clicked.connect(self.create_converts)
+        self.converts.textChanged.connect(self.line_edit_signal)
+        grid_layout = QGridLayout()
+        grid_layout.addWidget(self.get_directory_path, 0, 0)
+        grid_layout.addWidget(self.directory_path, 0, 1)
+        grid_layout.addWidget(self.iteration_label, 1, 0)
+        grid_layout.addWidget(self.converts, 1, 1)
+        grid_layout.addWidget(self.date_label, 2, 0)
+        grid_layout.addWidget(self.input_date, 2, 1)
+        grid_layout.addWidget(self.create_IK, 3, 0, 1, 2)
+        grid_layout.addWidget(self.progressbar, 4, 0, 1, 2)
+        widget = QWidget()
+        widget.setLayout(grid_layout)
+        self.setCentralWidget(widget)
+        self.iteration_label.setText('Количество конвертов')
+        self.date_label.setText('Введите дату')
+        self.create_IK.setEnabled(False)
+        self.get_directory_path.setToolTip('Выберите каталог для сохранения ИК')
+        self.directory_path.setToolTip('Можно вставить путь или выбрать с помощью кнопки')
+        self.converts.setToolTip('Количество необходимых конвертов')
+        self.create_IK.setToolTip('Введите количество необходимых конвертов')
+        self.input_date.setToolTip('Дата должна быть не ранее текущего ОД')
+        self.threadpool = QThreadPool()
+        self.iteration_label = ''
+        self.iteration_count = ''
+        # self.start_path = 'C:/install'
+        self.start_path = pathlib.Path.cwd()
+        self.envelope_path = self.start_path.joinpath('sample/envelope.xml')
+        self.routeinfo_path = self.start_path.joinpath('sample/RouteInfo.xml')
+        self.ed421_path = self.start_path.joinpath('sample/ED421.xml')
+        self.directory_path.setText(str(self.start_path))
 
+    '''Это от моего старого интерфейса
     def setupUi(self):
         self.setWindowTitle("Freezing GUI")
         self.resize(300, 150)
@@ -78,16 +120,25 @@ class Window(QMainWindow):
         layout.addWidget(self.stepLabel)
         layout.addWidget(self.longRunningBtn)
         self.centralWidget.setLayout(layout)
+    '''
 
-    def countClicks(self):
-        self.clicksCount += 1
-        self.clicksLabel.setText(f"Counting: {self.clicksCount} clicks")
+    def get_directory(self):
+        self.start_path = QFileDialog.getExistingDirectory(self, caption='Выбрать файл', directory='C:/')
+        self.directory_path.setText(self.start_path)
 
 
-    def reportProgress(self, n):
+# зачем эта функция? Из шаблона Димы
+    def line_edit_signal(self, value):
+        if self.converts.text() == '':
+            self.create_IK.setEnabled(False)
+        else:
+            self.create_IK.setEnabled(True)
+            self.iteration_count = value
+
+    def reportProgress(self, n):  # Это старое. Надо проверить
         self.stepLabel.setText(f"Long-Running Step: {n}")
 
-    def get_ed(self):
+    def get_ed(self):  # Это старое. Надо проверить
         get_ed421 = QFileDialog.getOpenFileName(self, caption='Выберите ED421', directory=os.getcwd())
         self.status.showMessage(f'Выбран файл: {get_ed421}')
         size_status = len(self.status.currentMessage())
@@ -105,7 +156,7 @@ class Window(QMainWindow):
         self.get_dir = get_ed421
         print(f'self.get_dir from func is {self.get_dir}')
 
-    def path_to_file(self):
+    def path_to_file(self):  # Это старое. Надо проверить
         self.thread = QThread()
         self.worker_b = Worker()
         self.worker_b.moveToThread(self.thread)
@@ -121,7 +172,7 @@ class Window(QMainWindow):
         # else:
         #     self.label_get_dir.setText('Directory for copying is not selected yet')
 
-    def runLongTask(self):
+    def runLongTask(self):  # Это старое. Но это что-то от многопоточности
         self.thread = QThread()
         self.worker = Worker()
         self.worker.moveToThread(self.thread)
@@ -145,8 +196,25 @@ class Window(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    style = """
+            QMainWindow {
+                /*background-color: Red;*/
+            }
+            QProgressBar {
+                border: 1px solid grey;
+                border-radius: 5px;
+                text-align: center;
+            }
+            QProgressBar::chunk {
+                background-color: #05B8CC;
+                width: 20px;
+            }
+            """
+    app.setStyleSheet(style)
     win = Window()
     win.show()
     sys.exit(app.exec())
 
 # TODO: поместить в строку со статусом время выполнения операции
+# TODO: что делает кнопка выбор каталога? Это каталог для сохранения ИК?
+# TODO: адаптировать размер окна под длину текста в поле для ввода каталога
