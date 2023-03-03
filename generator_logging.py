be65abe3340a5d6140be400bed1efed183e4216d
Steelglowhawk <steel_hawk@list.ru>
2023-03-03 06:50:19 +0500

    Подготовлено под использование в качестве модуля

--- generator_logging.py
+++ generator_logging.py
@@ -2,6 +2,17 @@ import logging
 import pathlib
 
 
-log_path = pathlib.Path.cwd().joinpath('log')
-logging.basicConfig(filename=log_path, level=logging.DEBUG, format=' %(asctime)s - %(leveltime)s - %(message)s')
-logging.debug('Start of program')
+def log_event(message):
+	log_dir_name = 'logs'
+	log_file_name = 'log.log'
+	pathlib.Path.cwd().joinpath(log_dir_name).mkdir(exist_ok=True)
+	log_dir_path = pathlib.Path.cwd().joinpath(log_dir_name)
+	log_file_path = pathlib.Path(log_dir_path).joinpath(log_file_name)
+	logging.disable(logging.DEBUG)
+	logging.basicConfig(filename=log_file_path, level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+	logging.basicConfig(filename=log_file_path, level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+	logging.info(message)
+
+
+if __name__ == '__main__':
+	log_event('Alarm 001')
